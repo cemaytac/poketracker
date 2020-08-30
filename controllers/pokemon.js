59db8d8ccb74942ae6199c144b6180c1f6abef39
cem aytac <cenko@MacBook-Air-3.local>
2020-08-29 22:16:48 -0400

    post search results of pokedex

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -4,13 +4,30 @@ const User = require('../models/user')
 
 module.exports = {
   pokedex,
+  pokeSearch
 };
+
+function pokeSearch(req, res) {
+  User.find({})
+    .then((users) => {
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${req.body.search.toLowerCase()}`)
+        .then(response => {
+          console.log(response.data.species)
+          res.render('pokemon/pokedexPage', {
+            pokemon: response.data,
+            user: req.user,
+            users
+          })
+        })
+    })
+}
 
 function pokedex(req, res) {
   User.find({})
     .then((users) => {
       res.render('pokemon/pokedexPage', {
         title: 'Pokedex',
+        pokemon: null,
         user: req.user,
         users
       })
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const pokemonCtrl = require('../controllers/pokemon');
 
 router.get('/pokedex', isLoggedIn, pokemonCtrl.pokedex);
+router.post('/pokeSearch', isLoggedIn, pokemonCtrl.pokeSearch);
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next();
