745e78641787b0cb47277cc0889560c1f610ae91
cem aytac <cenko@MacBook-Air-3.local>
2020-09-03 11:57:02 -0400

    edit volume for audio files

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,5 +1,7 @@
 const pokeSound = new Audio("/audio/pokeball.mp3")
+pokeSound.volume = 0.1;
 const clickA = new Audio("/audio/pressA.wav")
+clickA.volume = 0.1;
 
 function pokeballSound() {
   pokeSound.play();
